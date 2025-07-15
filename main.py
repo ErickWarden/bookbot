76a0938940391c53521cb431226b699f35babdab
ErickWarden <erickwarden@outlook.com>
2025-07-15 09:51:11 -0500

    finished book bot w/ sys.argvs function for any text

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
-from stats import count_words
+import sys
+from stats import (
+    count_words, 
+    count_characters, 
+    characters_list, 
+    sort_on
+)
 
-with open("./books/frankenstein.txt") as f:
-    file_contents = f.read()
-    number_of_words = count_words(file_contents)
-    print(f"{number_of_words} words found in the document")
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+
+    book_path = sys.argv[1]
+    file_contents = get_book_text(book_path)
+
+    # Call two functions to count both the number of words, and number of characters in the book.
+    word_count = count_words(file_contents)
+    char_count = count_characters(file_contents)
+    char_count_list = characters_list(char_count)
+    char_count_list.sort(reverse=True, key=sort_on)
+
+    # Format the char_count_list to look nice.
+    ccl_print = ""
+    for item in char_count_list:
+        ccl_print += f"{item['char']}: {item['num']} \n"
+
+    # Print final report, after formating.
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    print(ccl_print.rstrip())
+    print("============= END ===============")
     
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+main()
--- stats.py
+++ stats.py
@@ -1,3 +1,24 @@
 def count_words(book_text):
     word_list = book_text.split( )
     return len(word_list)
+
+def count_characters(book_text):
+    data_text = book_text.lower()
+    letter_dictionary = {}
+    for char in data_text:
+        if char in letter_dictionary:
+            letter_dictionary[char] += 1
+        else:
+            letter_dictionary[char] = 1
+    return letter_dictionary
+
+def characters_list(letter_dictionary):
+        dict_list = []
+        for c, n in letter_dictionary.items():
+             if c.isalpha():
+                new_dict = {"char": c, "num": n}
+                dict_list.append(new_dict)
+        return dict_list
+
+def sort_on(items):
+    return items["num"]
